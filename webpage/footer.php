17801eeed10919c0cdc269e109ab796dca5045e6
Travis Desell <travis.desell@gmail.com>
2013-02-05 13:32:19 -0600

    Added footer and thumbnails

---
+++ webpage/footer.php
@@ -0,0 +1,18 @@
+<?php
+
+function print_footer() {
+echo "
+    <!-- Footer
+    ================================================== -->
+    <footer class='footer'>
+        <div class='container'>
+            <center>
+            <p>Designed by <a href='http://people.cs.und.edu'>Travis Desell</a> with much help from <a href='http://twitter.github.com/bootstrap/getting-started.html'>Twitter's Bootstrap</a>.</p>
+            <p>&copy; Travis Desell, Susan Ellis-Felege and the University of North Dakota 2013</p>
+            </center>
+        </div>
+    </footer>
+    ";
+}
+
+?>
