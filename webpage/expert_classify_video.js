629702f415cf63a583de006a7b7246eb075fdd2b
Travis Desell <travis.desell@gmail.com>
2013-08-03 17:03:09 -0500

    Added expert classification webpage

---
+++ webpage/expert_classify_video.js
@@ -0,0 +1,290 @@
+$(document).ready(function () {
+
+    var location_id = -1;
+    var species_id = -1;
+    var animal_id = -1;
+    var video_min = 0;
+    var video_count = 10;
+    var event_ids = {};
+
+    $('.species-dropdown').click(function() {
+        var new_species_id = $(this).attr("species_id");
+
+        if (new_species_id !== species_id) {
+            species_id = new_species_id;
+
+            if (species_id == 0) {
+                $('#species-button').html('Species <span class="caret"></span>');
+            } else if (species_id == 1) {
+                $('#species-button').html('Sharp-tailed Grouse <span class="caret"></span>');
+            } else if (species_id == 2) {
+                $('#species-button').html('Interior Least Tern <span class="caret"></span>');
+            } else if (species_id == 3) {
+                $('#species-button').html('Piping Plover <span class="caret"></span>');
+            }
+
+            load_animal_ids();
+            load_videos();
+        }
+    });
+
+    $('.location-dropdown').click(function() {
+        var new_location_id = $(this).attr("location_id");
+
+        if (new_location_id !== location_id) {
+            location_id = new_location_id;
+
+            if (location_id == 0) {
+                $('#location-button').html('Location <span class="caret"></span>');
+            } else if (location_id == 1) {
+                $('#location-button').html('Belden, ND <span class="caret"></span>');
+            } else if (location_id == 2) {
+                $('#location-button').html('Blaisdell, ND <span class="caret"></span>');
+            } else if (location_id == 3) {
+                $('#location-button').html('Lostwood Wildlife Refuge, ND <span class="caret"></span>');
+            } else if (location_id == 4) {
+                $('#location-button').html('Missouri River, ND <span class="caret"></span>');
+            }
+
+            load_animal_ids();
+            load_videos();
+        }
+    });
+
+    load_animal_ids();
+
+    function load_animal_ids() {
+        animal_id = -1
+        $('#animal-id-button').html("Animal ID <span class='caret'></span>");
+
+        var submission_data = {
+                                species_id : species_id,
+                                location_id : location_id
+                              };
+
+        $.ajax({
+            type: 'POST',
+            url: './get_animal_ids.php',
+            data : submission_data,
+            dataType : 'text',
+            success : function(response) {
+//                console.log("the response was:\n" + response);
+                $("#animal-id-dropdown-menu").html(response);
+                enable_animal_id_dropdown();
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            },
+            async: true
+        });
+    }
+
+    function enable_animal_id_dropdown() {
+        $('.animal-id-dropdown').click(function() {
+            var new_animal_id = $(this).attr("animal_id");
+
+            if (new_animal_id !== animal_id) {
+                animal_id = new_animal_id;
+
+                if (animal_id == 0) {
+                    $('#animal-id-button').html('Animal ID <span class="caret"></span>');
+                } else {
+                    $('#animal-id-button').html(animal_id + ' <span class="caret"></span>');
+                }
+                load_videos();
+            }
+        });
+    }
+
+    load_videos();
+
+    function load_videos() {
+        var submission_data = {
+                                species_id : species_id,
+                                location_id : location_id,
+                                animal_id : animal_id,
+                                video_min : video_min,
+                                video_count : video_count
+                              };
+
+        $.ajax({
+            type: 'POST',
+            url: './get_expert_videos.php',
+            data : submission_data,
+            dataType : 'text',
+            success : function(response) {
+//                console.log("the response was:\n" + response);
+                $("#video-list-placeholder").html(response);
+                enable_accordion();
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            },
+            async: true
+        });
+
+        $.ajax({
+            type: 'POST',
+            url: './get_expert_count_nav.php',
+            data : submission_data,
+            dataType : 'text',
+            success : function(response) {
+//                console.log("the response was:\n" + response);
+                $("#videos-nav-placeholder").html(response);
+                init_dropdown();
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            },
+            async: true
+        });
+    }
+
+
+    function enable_accordion() {
+        $('.accordion-toggle').click(function(ev) {
+//            console.log("clicked an accordion toggle with href: " + $(this).attr('href'));
+
+            if ($( $(this).attr('href') + "_inner" ).html().indexOf('uninitialized') != -1) {
+                var target = $(this).attr('href') + "_inner";
+                $( target ).html("<p>Loading...</p>");
+
+                var submission_data = {
+                                        video_id : $( $(this).attr('href') ).attr("video_id"),
+                                        video_file : $( $(this).attr('href') ).attr("video_file")
+                                      };
+
+                $.ajax({
+                    type: 'POST',
+                    url: './get_expert_video.php',
+                    data : submission_data,
+                    dataType : 'text',
+                    success : function(response) {
+        //                console.log("the response was:\n" + response);
+                        $(target).html(response);
+
+                        $('.event-time-textbox').click(function() {
+                            var video_id = $(this).attr("video_id");
+//                            console.log("setting text for #wildlife-video-" + video_id);
+
+                            var time = Math.floor( $("#wildlife-video-" + video_id).get(0).currentTime );
+
+                            var hours = parseInt( time / 3600 ) % 24;
+                            var minutes = parseInt( time / 60 ) % 60;
+                            var seconds = time % 60;
+
+                            var result = (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds  < 10 ? "0" + seconds : seconds);
+
+//                            console.log( "Time: " + result);
+                            $(this).val( result );
+                        });
+
+                        $('.event-dropdown').click(function(ev) {
+                            var event_id = $(this).attr("event_id");
+                            var video_id = $(this).attr("video_id");
+
+                            event_ids[video_id] = event_id;
+
+//                            console.log("target = #event-button-" + video_id);
+//                            console.log("event_ids = " + JSON.stringify(event_ids));
+
+                            if (event_id === '0') {
+                                $('#event-button-' + video_id).html('Event <span class="caret"></span>');
+                            } else if (event_id === '1') {
+                                $('#event-button-' + video_id).html('Bird Leave <span class="caret"></span>');
+                            } else if (event_id === '2') {
+                                $('#event-button-' + video_id).html('Bird Return <span class="caret"></span>');
+                            } else if (event_id === '3') {
+                                $('#event-button-' + video_id).html('Predator <span class="caret"></span>');
+                            } else if (event_id === '4') {
+                                $('#event-button-' + video_id).html('Other Animal <span class="caret"></span>');
+                            } else if (event_id === '5') {
+                                $('#event-button-' + video_id).html('Nest Defense <span class="caret"></span>');
+                            } else if (event_id === '6') {
+                                $('#event-button-' + video_id).html('Nest Success <span class="caret"></span>');
+                            }
+
+                            ev.preventDefault();
+//                            ev.stopPropagation();
+                        });
+
+                    },
+                    error : function(jqXHR, textStatus, errorThrown) {
+                        alert(errorThrown);
+                    },
+                    async: true
+                });
+            }
+
+            $( $(this).attr('href') ).collapse('toggle');
+    //            console.log("toggle: " + $( $(this).attr('href') ).toggled);
+
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+    }
+
+    function init_dropdown() {
+        $('.video-nav-list').click(function(ev) {
+                var new_min = $(this).attr("id");
+
+                new_min = new_min.substring(11);
+
+                if (video_min != new_min) {
+                    video_min = new_min;
+                    load_videos(false);
+                }
+
+                ev.preventDefault();
+                ev.stopPropagation();
+        });
+
+        $('#go-to-button').button();
+        $('#go-to-button').click(function(ev) {
+            var value = $('#go-to-textbox').val();
+
+            if (Math.floor(value) == value && $.isNumeric(value)) {
+//                console.log("value is an integer!: " + value);
+                if (video_min != value) {
+                    video_min = value;
+                    video_min = video_min - (video_min % video_count);
+                    load_videos();
+                }
+            }
+        });
+
+
+        $('#display-5-dropdown').click(function(ev) {
+            if (video_count != 5) {
+                video_count = 5;
+                load_videos();
+            }
+
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+
+        $('#display-10-dropdown').click(function(ev) {
+            if (video_count != 10) {
+                video_count = 10;
+                load_videos();
+            }
+
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+
+        $('#display-20-dropdown').click(function(ev) {
+            if (video_count != 20) {
+                video_count = 20;
+                load_videos();
+            }
+
+            ev.preventDefault();
+            ev.stopPropagation();
+        });
+    }
+
+
+});
+
---
+++ webpage/get_expert_video.php
@@ -0,0 +1,75 @@
+<?php
+
+$video_id = mysql_real_escape_string($_POST['video_id']);
+$video_file = mysql_real_escape_string($_POST['video_file']);
+
+//echo "<p>Got this for video: $video_id, and file: $video_file</p>";
+
+echo "
+    <div class='row-fluid'>
+        <div class='span6'>
+            <video style='width:100%;' id='wildlife-video-$video_id' controls='controls' preload='auto'>
+                <source src='http://wildlife.und.edu/$video_file' type='video/mp4'></source>
+                <!-- <source src='http://wildlife.und.edu/$segment_filename.ogv' type='video/ogg'></source> -->
+                This video requires a browser that supports HTML5 video.
+            </video>
+        </div>";
+
+echo "  <div class='span6'>
+            <div class='row-fluid'>";
+
+echo "<table class='table table-striped table-bordered table-condensed'>
+        <thead>
+            <th>Event</th>
+            <th>Time</th>
+            <th>Comments</th>
+        </thead>";
+
+
+
+for ($i = 0; $i < 5; $i++) {
+   echo "<tr> <td>Predator</td> <td>11:32:02</td> <td>Badger</td> <td style='padding-top:0px; padding-bottom:0px; width:25px;'> <button class='btn btn-small btn-danger pull-right' id='remove-observation-button-$i' style='margin-top:3px; margin-bottom:0px; padding-top:0px; padding-bottom:0px;'> - </button> </td> </tr>"; 
+}
+
+echo "</table>";
+
+echo "      </div>
+
+            <div class='row-fluid'>
+                <div class='btn-group'>
+                    <button type='button' class='btn btn-small btn-default dropdown-toggle' data-toggle='dropdown' id='event-button-$video_id' style='margin-top:-7px;'>
+                        Event <span class='caret'></span>
+                    </button>
+                    <ul class='dropdown-menu' id='event-dropdown-$video_id'>
+                        <li><a href='#' class='event-dropdown' event_id='0' video_id='$video_id' id='any-event-dropdown'>Any Event</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='1' video_id='$video_id' id='bird-leave-dropdown'>Bird Leave</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='2' video_id='$video_id' id='bird-return-dropdown'>Bird Return</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='3' video_id='$video_id' id='predator-dropdown'>Predator</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='4' video_id='$video_id' id='other-animal-dropdown'>Other Animal</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='5' video_id='$video_id' id='nest-defense-dropdown'>Nest Defense</a></li>
+                        <li><a href='#' class='event-dropdown' event_id='6' video_id='$video_id' id='nest-success-dropdown'>Nest Success</a></li>
+                    </ul>
+                </div>
+
+                <input type='text' id='event-time-$video_id' video_id='$video_id' class='event-time-textbox' style='width:15%; margin-top:0px; padding-bottom:0px; margin-left:2px; margin-right:10px' value='Event Time'></input>
+
+                <button class='btn btn-small btn-primary pull-right' id='submit-observation-button' style='margin-right:4px;'> + </button>
+            </div>
+
+            <div class='row-fluid'>
+                <div class='span12'>
+                    <p style='padding-top:8px; margin-bottom:0px'>
+                    Comments:
+                    </p>
+                </div>
+            </div>
+
+            <div class='row-fluid' style='padding-top:0px margin-top:0px;'>
+                <textarea class='span12' name='comments' id='comments-$video_id'/>
+            </div>
+        </div>
+    </div>";
+
+
+
+?>
