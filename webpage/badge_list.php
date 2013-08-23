3a21018a7f6569ebee226ae7a78599517248d680
Travis Desell <travis.desell@gmail.com>
2013-08-23 02:04:09 -0500

    Updated the watched videos page. Added common badge functionality.

---
+++ webpage/badge_list.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once('/projects/wildlife/html/inc/util.inc');
+
+require_once('/home/tdesell/wildlife_at_home/webpage/navbar.php');
+require_once('/home/tdesell/wildlife_at_home/webpage/footer.php');
+require_once('/home/tdesell/wildlife_at_home/webpage/wildlife_db.php');
+require_once('/home/tdesell/wildlife_at_home/webpage/my_query.php');
+require_once('/home/tdesell/wildlife_at_home/webpage/display_badges.php');
+
+$bootstrap_scripts = file_get_contents("/home/tdesell/wildlife_at_home/webpage/bootstrap_scripts.html");
+
+echo "
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <title>Wildlife@Home: Badge Descriptions</title>
+
+    <!-- For bootstrap -->
+    $bootstrap_scripts
+
+    <script type='text/javascript' src='user_video_list.js'></script>
+
+    <style>
+    body {
+        padding-top: 60px;
+    }
+
+    @media (max-width: 979px) {
+        body {
+            padding-top: 0px;
+        }
+    }
+
+        .well {
+           position: relative;
+           margin: 15px 5px;
+           padding: 39px 19px 14px;
+           *padding-top: 19px;
+           border: 1px solid #ddd;
+           -webkit-border-radius: 4px;
+           -moz-border-radius: 4px;
+           border-radius: 4px; 
+        }
+
+        .tab {
+           position: absolute;
+           top: -1px;
+           left: -1px;
+           padding: 3px 7px;
+           font-size: 14px;
+           font-weight: bold;
+           background-color: #f5f5f5;
+           border: 1px solid #ddd;
+           color: #606060; 
+           -webkit-border-radius: 4px 0 4px 0;
+           -moz-border-radius: 4px 0 4px 0;
+           border-radius: 4px 0 4px 0;
+        }
+
+        .title {
+            text-align: center;
+           position: absolute;
+           top: -1px;
+           left: -1px;
+           width: 100%;
+           padding: 3px 0px 0px 0px;
+           font-size: 14px;
+           font-weight: bold;
+           background-color: #f5f5f5;
+           border: 1px solid #ddd;
+           color: #606060; 
+           -webkit-border-radius: 4px 4px 0px 0px;
+           -moz-border-radius: 4px 4px 0px 0px;
+           border-radius: 4px 4px 0px 0px;
+        }
+    </style>
+";
+
+
+echo "
+</head>
+<body>";
+
+
+$active_items = array(
+                    'home' => '',
+                    'watch_video' => '',
+                    'message_boards' => '',
+                    'preferences' => '',
+                    'about_wildlife' => '',
+                    'community' => 'active'
+                );
+
+print_navbar($active_items);
+
+echo "<div class='well well-small' style='padding-top:10px; padding-bottom:10px;'>";
+echo "<div class='container'>";
+echo "<div class='span12' style='margin-left:0px'>";
+
+echo "<p style='margin-top:5px; margin-bottom:5px;'>You can earn different badges for watching video or volunteering your computer to crunch workunits.  This page describes the different badges and how you can unlock them.</p>";
+echo "</div>";
+echo "</div>";
+echo "</div>";
+
+print_badge_table();
+
+print_footer();
+
+echo "
+</body>
+</html>
+";
+
+?>
--- webpage/get_video_count_nav.php
+++ webpage/get_video_count_nav.php
@@ -9,86 +9,68 @@ require_once('/home/tdesell/wildlife_at_home/webpage/generate_count_nav.php');
 $video_min = mysql_real_escape_string($_POST['video_min']);
 $video_count = mysql_real_escape_string($_POST['video_count']);
 
-$species_id = mysql_real_escape_string($_POST['species_id']);
-$location_id = mysql_real_escape_string($_POST['location_id']);
-
-$filters = $_POST['filters'];
-
-$new_filter = '';
-
-if ($filters['interesting'] == 'yes')           $new_filter .= " AND observations.interesting > 0";
-if ($filters['corrupt'] == 'yes')               $new_filter .= " AND observations.corrupt > 0";
-if ($filters['too_dark'] == 'yes')              $new_filter .= " AND observations.too_dark > 0";
-
-if ($filters['invalid'] == 'yes')               $new_filter .= " AND observations.status = 'INVALID'";
-if ($filters['bird_presence'] == 'yes')      $new_filter .= " AND observations.bird_presence > 0";
-if ($filters['bird_absence'] == 'yes')      $new_filter .= " AND observations.bird_absence > 0";
-if ($filters['chick_presence'] == 'yes')     $new_filter .= " AND observations.chick_presence > 0";
-if ($filters['predator_presence'] == 'yes')  $new_filter .= " AND observations.predator_presence > 0";
-if ($filters['nest_defense'] == 'yes')       $new_filter .= " AND observations.nest_defense > 0";
-if ($filters['nest_success'] == 'yes')       $new_filter .= " AND observations.nest_success > 0";
-if ($filters['bird_return'] == 'yes')        $new_filter .= " AND observations.bird_return > 0";
-if ($filters['bird_leave'] == 'yes')         $new_filter .= " AND observations.bird_leave > 0";
-
-if ($filters['invalid'] == 'unsure')               $new_filter .= " AND observations.status = 'UNVALIDATED'";
-if ($filters['bird_presence'] == 'unsure')      $new_filter .= " AND observations.bird_presence = 0";
-if ($filters['bird_absence'] == 'unsure')      $new_filter .= " AND observations.bird_absence = 0";
-if ($filters['chick_presence'] == 'unsure')     $new_filter .= " AND observations.chick_presence = 0";
-if ($filters['predator_presence'] == 'unsure')  $new_filter .= " AND observations.predator_presence = 0";
-if ($filters['nest_defense'] == 'unsure')       $new_filter .= " AND observations.nest_defense = 0";
-if ($filters['nest_success'] == 'unsure')       $new_filter .= " AND observations.nest_success = 0";
-if ($filters['bird_return'] == 'unsure')        $new_filter .= " AND observations.bird_return = 0";
-if ($filters['bird_leave'] == 'unsure')         $new_filter .= " AND observations.bird_leave = 0";
-
-if ($filters['invalid'] == 'no')               $new_filter .= " AND (observations.status = 'VALID' || observations.status = 'CANONICAL')";
-if ($filters['bird_presence'] == 'no')      $new_filter .= " AND observations.bird_presence < 0";
-if ($filters['bird_absence'] == 'no')      $new_filter .= " AND observations.bird_absence < 0";
-if ($filters['chick_presence'] == 'no')     $new_filter .= " AND observations.chick_presence < 0";
-if ($filters['predator_presence'] == 'no')  $new_filter .= " AND observations.predator_presence < 0";
-if ($filters['nest_defense'] == 'no')       $new_filter .= " AND observations.nest_defense < 0";
-if ($filters['nest_success'] == 'no')       $new_filter .= " AND observations.nest_success < 0";
-if ($filters['bird_return'] == 'no')        $new_filter .= " AND observations.bird_return < 0";
-if ($filters['bird_leave'] == 'no')         $new_filter .= " AND observations.bird_leave < 0";
-
-$filter = $new_filter;
-
-if ($species_id > 0) $filter .= " AND species_id = $species_id";
-if ($location_id > 0) $filter .= " AND location_id = $location_id";
-
-$display_nav_numbers = true;
-if (strlen($filter) < 5) {
-    $display_nav_numbers = false;
-}
-
-
-//error_log("the filter is: " . $filter);
-
-
 if ($video_min == NULL) $video_min = 0;
 if ($video_count == NULL) $video_count = 5;
-
 
 $user = get_logged_in_user();
 $user_id = $user->id;
 
-ini_set("mysql.connect_timeout", 300);
-ini_set("default_socket_timeout", 300);
+$filters = array();
+if (array_key_exists('filters', $_POST)) {
+    $filters = $_POST['filters'];
+}
 
-$wildlife_db = mysql_connect("wildlife.und.edu", $wildlife_user, $wildlife_passwd);
-mysql_select_db("wildlife_video", $wildlife_db);
+$filter = '';
+
+$reported_filter = '';
+
+foreach ($filters as $key => $value) {
+//    error_log("    '$key' => '$value'");
+
+    if ($key == 'report_status') {
+        $reported_filter .= " vs2.report_status = '" . mysql_real_escape_string($value) . "' AND ";
+    } else {
+        if ($value == 'VALID or CANONICAL') {
+            $filter .= " AND (observations." . mysql_real_escape_string($key) . " = 'VALID' OR observations." . mysql_real_escape_string($key) . " = 'CANONICAL') ";
+        } else if (!is_numeric($value)) {
+            $filter .= " AND observations." . mysql_real_escape_string($key) . " = '" . mysql_real_escape_string($value) . "' ";
+        } else {
+            $filter .= " AND observations." . mysql_real_escape_string($key) . " = " . mysql_real_escape_string($value) . " ";
+        }
+    }
+}
+
+$display_nav_numbers = true;
+if (empty($filters)) {
+    $display_nav_numbers = false;
+    $max_items = 0;
+} else {
+    //error_log("the filter is: " . $filter);
+
+    ini_set("mysql.connect_timeout", 300);
+    ini_set("default_socket_timeout", 300);
+
+    $wildlife_db = mysql_connect("wildlife.und.edu", $wildlife_user, $wildlife_passwd);
+    mysql_select_db("wildlife_video", $wildlife_db);
+
+    $query = "";
+    if (array_key_exists('all_users', $_POST)) {
+        $reported_filter = substr($reported_filter, 0, -4);
+        $query = "SELECT count(id) FROM video_segment_2 vs2 WHERE $reported_filter";
+    } else {
+        $query = "SELECT count(id) FROM video_segment_2 vs2 WHERE $reported_filter EXISTS (SELECT id FROM observations WHERE user_id = $user_id $filter AND observations.video_segment_id = vs2.id)";
+    }
+
+    //echo "<!-- $query -->\n";
 
 
-$query = "SELECT count(id) FROM video_segment_2 vs2 WHERE EXISTS (SELECT id FROM observations WHERE user_id = $user_id $filter AND observations.video_segment_id = vs2.id)";
+    $result = attempt_query_with_ping($query, $wildlife_db);
+    if (!$result) die ("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "\nquery: $query\n");
 
-//echo "<!-- $query -->\n";
+    $row = mysql_fetch_assoc($result);
 
-
-$result = attempt_query_with_ping($query, $wildlife_db);
-if (!$result) die ("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "\nquery: $query\n");
-
-$row = mysql_fetch_assoc($result);
-
-$max_items = $row['count(id)'];
+    $max_items = $row['count(id)'];
+}
 
 generate_count_nav($max_items, $video_min, $video_count, $display_nav_numbers);
 
