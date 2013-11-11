941f7054677e433e1630133f2b585197ba2af261
Kyle Goehner <KyleA.Goehner@gmail.com>
2013-11-10 20:00:50 -0600

    Updated assimilator

--- boinc_daemons/wildlife_surf_collect_assimilation_policy.cxx
+++ boinc_daemons/wildlife_surf_collect_assimilation_policy.cxx
@@ -41,6 +41,8 @@
 
 #include <boost/algorithm/string.hpp>
 
+#include "wildlife_surf.hpp"
+
 using namespace std;
 
 #define mysql_query_check(conn, query) __mysql_check (conn, query, __FILE__, __LINE__)
@@ -53,7 +55,7 @@ void __mysql_check(MYSQL *conn, string query, const char *file, const int line) 
         ex_msg << "ERROR in MySQL query: '" << query.c_str() << "'. Error: " << mysql_errno(conn) << " -- '" << mysql_error(conn) << "'. Thrown on " << file << ":" << line;
         cerr << ex_msg.str() << endl;
         exit(1);
-    }   
+    }
 }
 
 MYSQL *wildlife_db_conn = NULL;
@@ -131,22 +133,44 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
         return 0;
     }
 
-    int retval = get_output_file_path(result, fi.path);
-    if (retval) {
-        log_messages.printf(MSG_CRITICAL, "wildlife_surf_collect_validation_policy: Failed to get output file path: %d %s\n", result.id, result.name);
-        exit(0);
-        return retval;
-    }
+    vector<EventType*> event_types;
+    try {
+        string events_str = parse_xml<string>(result.stderr_out, "event_names");
+        stringstream ss(events_str);
+        vector<string> event_names;
 
-    FileStorage fs(fi.path.c_str(), FileStorage::READ);
-    for (unsigned int i=0; i<event_names.size(); i++) {
-        EventType *temp = new EventType;
-        temp->name = event_names[i];
-        fs[event_names[i]] >> temp->descriptors;
-        event_types->push_back(temp);
-    }
+        string temp;
+        std::getline(ss, temp, '\n');
+        while(std::getline(ss, temp, '\n')) {
+            log_messages.printf(MSG_DEBUG, "Event name: %s\n", temp.c_str());
+            event_names.push_back(temp);
+        }
 
-    fs.release();
+        int retval = get_output_file_path(result, fi.path);
+        if (retval) {
+            log_messages.printf(MSG_CRITICAL, "wildlife_surf_collect_assimilation_policy: Failed to get output file path: %d %s\n", result.id, result.name);
+            exit(0);
+            return retval;
+        }
+
+        FileStorage fs(fi.path.c_str(), FileStorage::READ);
+        for (unsigned int i=0; i<event_names.size(); i++) {
+            EventType *temp = new EventType;
+            temp->name = event_names[i];
+            fs[event_names[i]] >> temp->descriptors;
+            event_types.push_back(temp);
+        }
+
+        fs.release();
+    } catch (string error_message) {
+        log_messages.printf(MSG_CRITICAL, "wildlife_surf_collect_assimilation_policy get_data_from_result([RESULT#%d %s]) failed with error: %s\n", result.id, result.name, error_message.c_str());
+        log_messages.printf(MSG_CRITICAL, "XML:\n%s\n", result.stderr_out);
+        result.outcome = RESULT_OUTCOME_VALIDATE_ERROR;
+        result.validate_state = VALIDATE_STATE_INVALID;
+
+        log_messages.printf(MSG_DEBUG, "Returning XML Error for %s\n", result.name);
+        return ERR_XML_PARSE;
+    }
 
     // Here we should have a list of event types.
 
@@ -161,7 +185,7 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
     uint32_t second_pos = result_name.find("_", first_pos);
 
     if (first_pos == string::npos || second_pos == string::npos) {
-        log_messages.printf(MSG_CRITICAL, "wildlife_assimilation_policy assimilate_handler failed with 'malformed result name error', result name: %s\n", result_name.c_str());
+        log_messages.printf(MSG_CRITICAL, "wildlife_surf_collect_assimilation_policy assimilate_handler failed with 'malformed result name error', result name: %s\n", result_name.c_str());
         exit(1);
     }
 
@@ -173,7 +197,7 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
     //  SELECT id, species_id, location_id FROM video_segment_2 WHERE video_id = video_id and number = i
 
     ostringstream full_video_query;
-    full_video_query << "SELECT species_id, location_id, start_time, duration_s FROM video_2 WHERE id = " << video_id << endl;
+    full_video_query << "SELECT species_id, location_id FROM video_2 WHERE id = " << video_id << endl;
 
     mysql_query_check(wildlife_db_conn, full_video_query.str());
     MYSQL_RES *video_result = mysql_store_result(wildlife_db_conn);
@@ -183,86 +207,19 @@ int assimilate_handler(WORKUNIT& wu, vector<RESULT>& results, RESULT& canonical_
     MYSQL_ROW full_video_row = mysql_fetch_row(video_result);
     int species_id = atoi(full_video_row[0]);
     int location_id = atoi(full_video_row[1]);
-    string start_time = full_video_row[2];
-    int duration_s = atoi(full_video_row[3]);
-
-    uint32_t calculated_segments = ceil(duration_s / 180.0);
-    cout << "duration_s: " << duration_s << ", segments: " << calculated_segments << ", p1.size(): " << p1.size() << endl;
-
-    if (p1.size() < (calculated_segments - 1) || p1.size() > calculated_segments) {
-        cout << "Wrong number of seconds" << endl;
-        mysql_free_result(video_result);
-        exit(1);
-        return 0;
-    }
-
-    for (uint32_t i = 0; i < p1.size(); i++) {
-        //determine video segment id for each 3 minute probability
-        ostringstream video_segment_query;
-        video_segment_query << "SELECT id FROM video_segment_2 WHERE video_id = " << video_id << " AND number = " << i;
-
-        cout << "getting video segments: '" << video_segment_query.str() << "'" << endl;
-
-        mysql_query_check(wildlife_db_conn, video_segment_query.str());
-        MYSQL_RES *video_segment_result = mysql_store_result(wildlife_db_conn);
-
-        if (video_segment_result == NULL) {
-            log_messages.printf(MSG_CRITICAL, "wildlife_assimilation_policy assimilate_handler failed with 'no matching video segment id', result name: %s\n", result_name.c_str());
-            log_messages.printf(MSG_CRITICAL, "\tvideo segment id = ? for video_id = %s and number = %u\n", video_id.c_str(), i);
-            exit(1);
-        }
-
-        MYSQL_ROW video_segment_row = mysql_fetch_row(video_segment_result);
-
-        if (!video_segment_row) {
-            cout << "could not get a video segment row" << endl;
-            exit(1);
-        }
-
-        int video_segment_id = atoi(video_segment_row[0]);
-
-        cout << "video segment id = " << video_segment_id << " for video_id = " << video_id << " and number = " << i << endl;
-
-
-        //Set the data for the classification for that three minute segment
-        ostringstream classifications_query;
-        classifications_query << "REPLACE INTO classifications "
-                              << " SET video_id = " << video_id
-                              << ", video_segment_id = " << video_segment_id
-                              << ", probability = " << p1[i]
-//                              << ", type = '" << detection_method << "'"
-//                              << ", detection = '" << detection_target << "'"
-                              << ", tag = '" << tag_str << "'"
-                              << ", species_id = " << species_id
-                              << ", location_id = " << location_id
-                              << ", start_time = (SELECT ADDTIME(\"" << start_time << "\", SEC_TO_TIME(" << i * 180 << ")))";
-
-        //this will allow multiple classifications with the same information
-
-        cout << "classifications query: '" << classifications_query.str() << "'" << endl;
-
-        mysql_query_check(wildlife_db_conn, classifications_query.str());
-        MYSQL_RES *classifications_result = mysql_store_result(wildlife_db_conn);
-
-        /*
-        if (classifications_result == NULL) {
-            log_messages.printf(MSG_CRITICAL, "wildlife_assimilation_policy assimilate_handler failed with 'could not insert classification', result name: %s\n", result_name.c_str());
-            exit(1);
-        }
-        */
-
-        cout << "freeing results" << endl;
-
-        mysql_free_result(video_segment_result);
-        mysql_free_result(classifications_result);
-
-        cout << "next iteration" << endl;
-    }
-
-    cout << "finished loop" << endl;
 
     mysql_free_result(video_result);
 
-    //Don't need to do anything, when the result is validated it gets inserted into the database directly
+    // Here we need to create/insert files for each event type into a stored
+    // directory structure.
+    for(vector<EventType*>::iterator it = event_types.begin(); it != event_types.end(); ++it) {
+        string filename = "/projects/wildlife/feature_files/" + tag_str + "/" + species_id + "/" + location_id + "/" + video_id + "/" + (*it)->id.c_str() + ".desc";
+        FileStorage outfile(filename, FileStorage::WRITE)
+        cerr << "Write: " << (*it)->id.c_str() << endl;
+        outfile << (*it)->id << (*it)->descriptors;
+        outfile.release();
+    }
+
+    exit(0);
     return 0;
 }
