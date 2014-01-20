b116c85d7e91c91c3526af894deb13ce52e31d07
Kyle Goehner <KyleA.Goehner@gmail.com>
2014-01-20 00:15:58 -0600

    Refactor

--- include/VideoType.hpp
+++ include/VideoType.hpp
@@ -1,25 +1,35 @@
-#ifndef VIDEO_TYPE_H
-#define VIDEO_TYPE_H
+#ifndef VIDEO_TYPE_HEADER
+#define VIDEO_TYPE_HEADER
 
+#include <vector>
 #include <opencv2/core/core.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+
+using namespace std;
 
 class VideoType {
 	 int width;
 	 int height;
-     cv::Rect watermarkedRect;
+     cv::Rect watermarkRect;
      cv::Rect timestampRect;
 
      void setWatermarkRect(cv::Point, cv::Point);
      void setTimestampRect(cv::Point, cv::Point);
 
     public:
-     void VideoType(int, int);
+     VideoType(int width, int height);
+
      void setWidth(int);
      void setHeight(int);
      int getWidth();
      int getHeight();
      cv::Rect getWatermarkRect();
      cv::Rect getTimestampRect();
+
+     vector<cv::KeyPoint> getCleanKeypoints(vector<cv::KeyPoint> keypoints);
+
+    private:
+     void loadType();
 };
 
 #endif
--- source/VideoType.cpp
+++ source/VideoType.cpp
@@ -1,7 +1,14 @@
+#include <vector>
+
 #include <opencv2/core/core.hpp>
+
 #include "VideoType.hpp"
 
-void VideoType::VideoType(int width, int height) {
+using namespace std;
+
+// Accessors
+
+VideoType::VideoType(int width, int height) {
     this->width = width;
     this->height = height;
     // Create rects?
@@ -16,11 +23,11 @@ void VideoType::setHeight(int pixels) {
 }
 
 void VideoType::setWatermarkRect(cv::Point topLeft, cv::Point bottomRight) {
-    this->watermarkRect = new cv::Rect(topLeft, bottomRight);
+    this->watermarkRect = cv::Rect(topLeft, bottomRight);
 }
 
 void VideoType::setTimestampRect(cv::Point topLeft, cv::Point bottomRight) {
-    this->timestampRect = new cv::Rect(topLeft, bottomRight);
+    this->timestampRect = cv::Rect(topLeft, bottomRight);
 }
 
 int VideoType::getWidth() {
@@ -32,18 +39,33 @@ int VideoType::getHeight() {
 }
 
 cv::Rect VideoType::getWatermarkRect() {
-    return ehis->watermarkRect;
+    return this->watermarkRect;
 }
 
 cv::Rect VideoType::getTimestampRect() {
     return this->timestampRect;
 }
 
+// Functions
+
+vector<cv::KeyPoint> VideoType::getCleanKeypoints(vector<cv::KeyPoint> keypoints) {
+    vector<cv::KeyPoint> cleanPoints;
+    for(int i=0; i<keypoints.size(); i++) {
+        cv::Point pt = keypoints.at(i).pt;
+        bool watermark = true;
+        bool timestamp = true;
+        if(!watermarkRect.contains(pt)) watermark = false;
+        if(!timestampRect.contains(pt)) timestamp = false;
+        if(!watermark && !timestamp) cleanPoints.push_back(keypoints.at(i));
+    }
+    return cleanPoints;
+}
+
 // This needs to be fixed to load in from a config file.
-void loadType() {
+void VideoType::loadType() {
     if(width == 704 && height ==480) {
         cv::Point watermarkTopLeft(12, 12);
-        cv::Point watermarkBottomTight(90, 55);
+        cv::Point watermarkBottomRight(90, 55);
         cv::Point timestampTopLeft(520, 415);
         cv::Point timestampBottomRight(680, 470);
         setWatermarkRect(watermarkTopLeft, watermarkBottomRight);
@@ -53,7 +75,7 @@ void loadType() {
         cv::Point watermarkBottomRight(90, 55);
         cv::Point timestampTopLeft(240, 190);
         cv::Point timestampBottomRight(335, 230);
-        b.watermark_rect = new cv::Rect(watermarkTopLeft, watermarkBottomRight);
-        b.timestamp_rect = new cv::Rect(timestampTopLeft, timestampBottomRight);
+        setWatermarkRect(watermarkTopLeft, watermarkBottomRight);
+        setTimestampRect(timestampTopLeft, timestampBottomRight);
     }
 }
