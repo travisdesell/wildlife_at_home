e74066ad1309793abf9c59187adb8c776c4d9c91
Kyle Goehner <KyleA.Goehner@gmail.com>
2014-04-09 08:47:49 -0500

    Fixed uninitialized values in time function

--- source/boinc_utils.cpp
+++ source/boinc_utils.cpp
@@ -39,7 +39,7 @@ int timeToSeconds(const string time) {
 
 double getTimeInSeconds() {
     time_t timer;
-    struct tm y2k;
+    struct tm y2k = {0};
     double seconds;
     y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
     y2k.tm_year = 100; y2k.tm_mon = 0; y2k.tm_mday = 1;
