a2d4d7d3c202a209be3d3357fbeab2b97127f1da
Kyle Goehner <Kyle.Goehner.2@my.und.edu>
2014-10-21 20:39:49 -0500

    Updated charts with descriptions and paramter descriptions.

--- webpage/correctness.php
+++ webpage/correctness.php
@@ -136,6 +136,17 @@ echo "
 
             <div id='chart_div' style='margin: auto; width: auto; height: 500px;'></div>
 
+            <h2>Parameters: (portion of the URL after a '?')</h2>
+            <dl>
+                <dt>buffer=</dt>
+                <dd>The error in either direction allowed for two events to be matched. The default value is 5.</dd>
+            </dl>
+            
+
+            <h2>Description:</h2>
+            <p>This scatterplot shows the </p>
+            <p>In order to collect this data we discard all vidoes that do not have an expert observation or the expert observation is invalid. This is done by getting a list of all event types and then counting the total number of user events that have a matchins event and dividing it by the number of user events of that type that have an valid expert observation for that video.</p>
+
         </div>
     </div>
 </div>
