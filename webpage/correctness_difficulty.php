830f13fdb42e88283a7feac7da8dec3a6e817ab1
Kyle Goehner <Kyle.Goehner.2@my.und.edu>
2014-10-29 11:40:25 -0500

    Update charts to work with new CSG framework.

--- webpage/correctness_difficulty.php
+++ webpage/correctness_difficulty.php
@@ -7,11 +7,8 @@ $cwd[__FILE__] = dirname(dirname($cwd[__FILE__]));
 //echo $cwd[__FILE__];
 require_once($cwd[__FILE__] . "/../citizen_science_grid/header.php");
 require_once($cwd[__FILE__] . "/../citizen_science_grid/navbar.php");
-//require_once($cwd[__FILE__] . "/../citizen_science_grid/news.php");
 require_once($cwd[__FILE__] . "/../citizen_science_grid/footer.php");
-//require_once($cwd[__FILE__] . "/../citizen_science_grid/uotd.php");
-require_once($cwd[__FILE__] . "/webpage/wildlife_db.php");
-require_once($cwd[__FILE__] . "/webpage/my_query.php");
+require_once($cwd[__FILE__] . "/../citizen_science_grid/my_query.php");
 
 print_header("Wildlife@Home: Duration vs Difficulty", "", "wildlife");
 print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
@@ -29,19 +26,12 @@ if (!isset($buffer)) {
     $buffer = 5;
 }
 
-$wildlife_db = mysql_connect("wildlife.und.edu", $wildlife_user, $wildlife_passwd);
-mysql_select_db("wildlife_video", $wildlife_db);
-
 $easy_watch_query = "SELECT user_id, video_id FROM watched_videos WHERE difficulty = 'easy'";
 $medium_watch_query = "SELECT user_id, video_id FROM watched_videos WHERE difficulty = 'medium'";
 $hard_watch_query = "SELECT user_id, video_id FROM watched_videos WHERE difficulty = 'hard'";
-$easy_watch_result = attempt_query_with_ping($easy_watch_query, $wildlife_db);
-$medium_watch_result = attempt_query_with_ping($medium_watch_query, $wildlife_db);
-$hard_watch_result = attempt_query_with_ping($hard_watch_query, $wildlife_db);
-if (!$easy_watch_result || !$medium_watch_result || !$hard_watch_result) {
-    error_log("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "/nquery: $easy_watch_query\n");
-    die("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "/nquery: $easy_watch_query\n");
-}
+$easy_watch_result = query_wildlife_video_db($easy_watch_query);
+$medium_watch_result = query_wildlife_video_db($medium_watch_query);
+$hard_watch_result = query_wildlife_video_db($hard_watch_query);
 
 echo "
 <div class='containder'>
@@ -73,18 +63,14 @@ function standard_deviation($sample){
     }
 }
 
-function getCorrectness($db, $user_id, $video_id, $buffer) {
+function getCorrectness($user_id, $video_id, $buffer) {
     $event_query = "SELECT event_id, to_seconds(start_time) AS start_sec, to_seconds(end_time) AS end_sec FROM timed_observations AS t JOIN observation_types AS e ON e.id = event_id WHERE expert = 0 AND user_id = $user_id AND video_id = $video_id AND start_time > 0 AND end_time > start_time AND EXISTS (SELECT * FROM timed_observations AS i WHERE t.video_id = i.video_id AND i.expert = 1 AND i.start_time > 0 AND i.end_time > i.start_time)";
-    $event_result = attempt_query_with_ping($event_query, $db);
-    if (!$event_result) {
-        error_log("MYSQL Error (" . mysql_errno($db) . "): " . mysql_error($db) . "/nquery: $event_query\n");
-        die("MYSQL Error (" . mysql_errno($db) . "): " . mysql_error($db) . "/nquery: $event_query\n");
-    }
-    $num_events = mysql_num_rows($event_result);
+    $event_result = query_wildlife_video_db($event_query);
+    $num_events = $event_result->num_rows;
     $num_match_events = 0;
 
     if ($num_events > 0) {
-        while ($event_row = mysql_fetch_assoc($event_result)) {
+        while ($event_row = $event_result->fetch_assoc()) {
             $event_id = $event_row['event_id'];
             $start_sec = $event_row['start_sec'];
             $end_sec = $event_row['end_sec'];
@@ -94,12 +80,8 @@ function getCorrectness($db, $user_id, $video_id, $buffer) {
             $end_sec_top = $end_sec - $buffer;
             $end_sec_bot = $end_sec + $buffer;
             $match_query = "SELECT * FROM timed_observations WHERE expert = 1 AND video_id = $video_id AND event_id = $event_id AND to_seconds(start_time) BETWEEN $start_sec_top AND $start_sec_bot AND to_seconds(end_time) BETWEEN $end_sec_top AND $end_sec_bot";
-            $match_result = attempt_query_with_ping($match_query, $db);
-            if (!$match_result) {
-                error_log("MYSQL Error (" . mysql_errno($db) . "): " . mysql_error($db) . "/nquery: $match_query\n");
-                die("MYSQL Error (" . mysql_errno($db) . "): " . mysql_error($db) . "/nquery: $match_query\n");
-            }
-            $num_matches = mysql_num_rows($match_result);
+            $match_result = query_wildlife_video_db($match_query);
+            $num_matches = $match_result->num_rows;
 
             if ($num_matches >= 1) {
                 $num_match_events += 1;
@@ -114,8 +96,8 @@ function getCorrectness($db, $user_id, $video_id, $buffer) {
 }
 
 $elements = array();
-while ($easy_watch_row = mysql_fetch_assoc($easy_watch_result)) {
-    array_push($elements, getCorrectness($wildlife_db, $easy_watch_row['user_id'], $easy_watch_row['video_id'], $buffer));
+while ($easy_watch_row = $easy_watch_result->fetch_assoc()) {
+    array_push($elements, getCorrectness($easy_watch_row['user_id'], $easy_watch_row['video_id'], $buffer));
 }
 sort($elements);
 $size = sizeof($elements);
@@ -136,8 +118,8 @@ if ($size > 0) {
 }
 
 $elements = array();
-while ($medium_watch_row = mysql_fetch_assoc($medium_watch_result)) {
-    array_push($elements, getCorrectness($wildlife_db, $medium_watch_row['user_id'], $medium_watch_row['video_id'], $buffer));
+while ($medium_watch_row = $medium_watch_result->fetch_assoc()) {
+    array_push($elements, getCorrectness($medium_watch_row['user_id'], $medium_watch_row['video_id'], $buffer));
 }
 sort($elements);
 $size = sizeof($elements);
@@ -158,8 +140,8 @@ if ($size > 0) {
 }
 
 $elements = array();
-while ($hard_watch_row = mysql_fetch_assoc($hard_watch_result)) {
-    array_push($elements, getCorrectness($wildlife_db, $hard_watch_row['user_id'], $hard_watch_row['video_id'], $buffer));
+while ($hard_watch_row = $hard_watch_result->fetch_assoc()) {
+    array_push($elements, getCorrectness($hard_watch_row['user_id'], $hard_watch_row['video_id'], $buffer));
 }
 sort($elements);
 $size = sizeof($elements);
