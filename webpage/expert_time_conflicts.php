86711e08e5e23548264def4647848bc893943ce9
Kyle Goehner <Kyle.Goehner.2@my.und.edu>
2014-11-03 12:46:08 -0600

    Fix expert conflict pages to work with csg

--- webpage/expert_time_conflicts.php
+++ webpage/expert_time_conflicts.php
@@ -7,11 +7,8 @@ $cwd[__FILE__] = dirname(dirname($cwd[__FILE__]));
 //echo $cwd[__FILE__];
 require_once($cwd[__FILE__] . "/../citizen_science_grid/header.php");
 require_once($cwd[__FILE__] . "/../citizen_science_grid/navbar.php");
-//require_once($cwd[__FILE__] . "/../citizen_science_grid/news.php");
 require_once($cwd[__FILE__] . "/../citizen_science_grid/footer.php");
-//require_once($cwd[__FILE__] . "/../citizen_science_grid/uotd.php");
-require_once($cwd[__FILE__] . "/webpage/wildlife_db.php");
-require_once($cwd[__FILE__] . "/webpage/my_query.php");
+require_once($cwd[__FILE__] . "/../citizen_science_grid/my_query.php");
 
 print_header("Wildlife@Home: Expert Time Conflict Table", "", "wildlife");
 print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
@@ -24,16 +21,9 @@ ini_set("default_socket_timeout", 300);
 // Get Parameters
 //parse_str($_SERVER['QUERY_STRING']);
 
-$wildlife_db = mysql_connect("wildlife.und.edu", $wildlife_user, $wildlife_passwd);
-mysql_select_db("wildlife_video", $wildlife_db);
-
 $query = "SELECT DISTINCT vid.animal_id, vid.watermarked_filename AS video_name, obs.video_id, ot.name AS event_name, obs.start_time, obs.end_time FROM timed_observations AS obs JOIN observation_types AS ot ON obs.event_id = ot.id JOIN video_2 AS vid ON vid.id = obs.video_id WHERE expert = 1 AND (obs.start_time <= 0 OR obs.start_time >= obs.end_time)";
 
-$result = attempt_query_with_ping($query, $wildlife_db);
-if (!$result) {
-    error_log("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "/nquery: $easy_query\n");
-    die("MYSQL Error (" . mysql_errno($wildlife_db) . "): " . mysql_error($wildlife_db) . "/nquery: $easy_query\n");
-}
+$result = query_wildlife_video_db($query);
 
 echo "
 <div class='containder'>
@@ -65,7 +55,7 @@ echo "
             data.addRows([
 ";
 
-while ($row = mysql_fetch_assoc($result)) {
+while ($row = $result->fetch_assoc()) {
     echo "['" . trim($row['animal_id']) . "'";
     echo ",'" . trim($row['video_id']) . "'";
     echo ",'" . trim(end(explode('/', $row['video_name']))) . "'";
