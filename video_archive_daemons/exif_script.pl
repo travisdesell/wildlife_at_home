7dbf8b8325cac338dac045b3e3569f2a912caa2d
jlovin <jlovin@undhpcvideo.itssint.und.edu>
2014-11-17 21:21:44 -0600

    Updated exif_script

--- video_archive_daemons/exif_script.pl
+++ video_archive_daemons/exif_script.pl
@@ -1,21 +1,82 @@
 #!/usr/bin/perl
 
+use strict;
 use Image::ExifTool qw(:Public);
 use Data::Dumper;
+use File::Find;
+use DBI;
+use Cwd 'abs_path'; 
 
-#create array of image files
-opendir(DH, '/share/wildlife/archive/hudson_bay_project/2014/COEI/C71/100RECNX') or die $!;
-my @files = grep /\.jpg/i , readdir(DH);
-closedir(DH);
+my $dir = "/share/wildlife/archive/hudson_bay_project/2014/LSGO";
 
-my $info;
+find(\&get_data, $dir);
 
-#for each image check if it is in the database, if it is not, add it to the database
-#for now just prints data
-foreach $file (@files)
+sub get_data
 {
-	$info = ImageInfo("/share/wildlife/archive/hudson_bay_project/2014/COEI/C71/100RECNX/$file");
-	print "\n$file: \n";
-	print "Date and time: ", $info->{'CreateDate'}, "\n",
-		"Temperature: ", $info->{'AmbientTemperature'}, "\n";
-}
+	my $file = $_;
+
+	#print $file;
+	
+
+
+	if($file =~ /\w*\.JPG/)
+	{
+		my $info = ImageInfo($file);
+
+		my $temp = ($info->{'AmbientTemperature'});
+		$temp =~ s/\D+//g;
+
+		my $abs_path = abs_path($file);
+
+		#get year from path
+		$abs_path =~ /hudson_bay_project\/(\d\d\d\d)\//;
+		my $year = $1;
+
+		#get species from path
+		$abs_path =~ /hudson_bay_project\/\d\d\d\d\/(\w+)\//;
+		my $species_name = $1;
+		my $species;
+		
+		if($species_name eq "COEI")
+		{
+			$species = 1;
+		}
+		elsif($species_name eq "something else")
+		{
+			$species = 2;
+		}
+		else
+		{
+			$species = 0;
+		}
+
+		#get camera id from path
+		$abs_path =~ /hudson_bay_project\/\d\d\d\d\/\w+\/(\w+-?\w*)\//;
+		my $camera_id = $1;
+
+		
+		#connect to database
+		my $driver = "mysql"; 
+		my $database = "wildlife_video";
+		my $dsn = "DBI:$driver:database=$database";
+		my $userid = "wildlife_user";
+		my $password = "gr0u\$e\$";
+
+		my $dbh = DBI->connect($dsn, $userid, $password ) or die "ERROR: Could not connect to database";
+
+		#add data to database
+		my $sth = $dbh->prepare("INSERT INTO images
+                       (temp, datetime, archive_filename, camera_id, species, year)
+                        values
+                       ('$temp', '$info->{'DateTimeOriginal'}', '$abs_path', '$camera_id', '$species', '$year')");
+		$sth->execute();    # or print $DBI::errstr;
+		$sth->finish();
+		#$dbh->commit or die $DBI::errstr;
+
+		#for now it just prints the data
+		#print "\n$file: \n";
+		#print "Date and time: ", $info->{'CreateDate'}, "\n",
+		#	"Temperature: ", $temp, "\n",
+		#	"Path: ", $abs_path, "\n";
+
+	}
