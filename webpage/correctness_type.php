15cda551cdb590bbb4989c7bcbf94ca30d75ccf8
Kyle Goehner <Kyle.Goehner.2@my.und.edu>
2015-01-12 15:04:15 -0600

    Updated correctess algorithms to specify a threshold and exact user to comapre against

--- webpage/correctness_type.php
+++ webpage/correctness_type.php
@@ -27,6 +27,10 @@ if (!isset($buffer)) {
     $buffer = 5;
 }
 
+if (!isset($threshold)) {
+    $threshold = 95;
+}
+
 $type_query = "SELECT id, name FROM observation_types";
 $type_result = query_wildlife_video_db($type_query, $wildlife_db);
 
@@ -61,7 +65,7 @@ echo "
 while ($type_row = $type_result->fetch_assoc()) {
     $type_id = $type_row['id'];
     $type_name = $type_row['name'];
-    $timed_query = "SELECT id FROM timed_observations AS t WHERE expert = 0 AND event_id = $type_id AND start_time > 0 AND end_time > start_time AND EXISTS (SELECT * FROM timed_observations AS i WHERE t.video_id = i.video_id AND i.expert = 1 AND i.start_time > 0 AND i.end_time > i.start_time)";
+    $timed_query = "SELECT id, video_id FROM timed_observations AS t WHERE expert = 0 AND event_id = $type_id AND start_time > 0 AND end_time > start_time AND EXISTS (SELECT * FROM timed_observations AS i WHERE t.video_id = i.video_id AND i.expert = 1 AND i.start_time > 0 AND i.end_time > i.start_time)";
     $timed_result = query_wildlife_video_db($timed_query);
     $num_events = $timed_result->num_rows;
     $buffer_match_events = 0;
@@ -69,15 +73,24 @@ while ($type_row = $type_result->fetch_assoc()) {
     $segmented_euclidean_match_events = 0;
     while ($timed_row = $timed_result->fetch_assoc()) {
         $obs_id = $timed_row['id'];
-        $buffer_correctness = getBufferCorrectness($obs_id, $buffer);
-        $euclidean_correctness = getEuclideanCorrectness($obs_id);
-        $segmented_euclidean_correctness = getSegmentedEuclideanCorrectness($obs_id);
+        $video_id = $timed_row['video_id'];
 
-        if ($euclidean_correctness > 0.95) {
+        $expert_query = "SELECT user_id FROM timed_observations WHERE video_id = $video_id AND expert = 1 LIMIT 1";
+        $expert_result = query_wildlife_video_db($expert_query);
+        $expert_id = -1;
+        while ($expert_row = $expert_result->fetch_assoc()) {
+            $expert_id = $expert_row['user_id'];
+        }
+
+        list($buffer_correctness, $buffer_specificity) = getBufferCorrectness($obs_id, $expert_id, $buffer);
+        list($euclidean_correctness, $euclidean_specificity) = getEuclideanCorrectness($obs_id, $expert_id, $threshold);
+        list($segmented_euclidean_correctness, $segmented_euclidean_specificity) = getSegmentedEuclideanCorrectness($obs_id, $expert_id, $threshold);
+
+        if ($euclidean_specificity) {
             $euclidean_match_events += $euclidean_correctness;
         }
         
-        if ($segmented_euclidean_correctness > 0.95) {
+        if ($segmented_euclidean_specificity) {
             $segmented_euclidean_match_events += $segmented_euclidean_correctness;
         }
 
