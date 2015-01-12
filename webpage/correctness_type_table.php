22bfc86eb15e879bec2b6afe5365f8f6186f5bdf
Kyle Goehner <Kyle.Goehner.2@my.und.edu>
2015-01-12 15:04:15 -0600

    Updated correctess algorithms to specify a threshold and exact user to comapre against

--- webpage/correctness_type_table.php
+++ webpage/correctness_type_table.php
@@ -11,7 +11,7 @@ require_once($cwd[__FILE__] . "/../citizen_science_grid/footer.php");
 require_once($cwd[__FILE__] . "/../citizen_science_grid/my_query.php");
 require_once($cwd[__FILE__] . "/webpage/correctness.php");
 
-print_header("Wildlife@Home: Correctness by Event Type and Species", "", "wildlife");
+print_header("Wildlife@Home: Correctness by Event Type", "", "wildlife");
 print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
 
 //echo "Header:";
@@ -34,56 +34,44 @@ if (!isset($threshold)) {
 $type_query = "SELECT id, name FROM observation_types";
 $type_result = query_wildlife_video_db($type_query, $wildlife_db);
 
-$species_query = "SELECT id, name FROM species";
-$species_result = query_wildlife_video_db($species_query, $wildlife_db);
-
-$species = array();
-while ($species_row = $species_result->fetch_assoc()) {
-    $species_id = $species_row['id'];
-    $species_name = $species_row['name'];
-    $species[$species_id] = $species_name;
-}
-ksort($species);
-
 echo "
 <div class='containder'>
     <div class='row'>
         <div class='col-sm-12'>
     <script type = 'text/javascript' src='https://www.google.com/jsapi'></script>
     <script type = 'text/javascript'>
-        google.load('visualization', '1.1', {packages:['corechart']});
+        google.load('visualization', '1.1', {packages:['table']});
         google.setOnLoadCallback(drawChart);
 
         function drawChart() {
             var container = document.getElementById('chart_div');
             var data = new google.visualization.DataTable();
             data.addColumn('string', 'Event Type');
-";
-
-foreach($species as $s_id => $s_name) {
-    echo "data.addColumn('number', '$s_name');";
-}
-
-echo "
-            data.addColumn({type: 'string', role: 'tooltip'});
+            data.addColumn('number', 'Total');
+            data.addColumn('number', 'Buffer True Positives');
+            data.addColumn('number', 'Buffer False Positives');
+            data.addColumn('number', 'Euclidean True Positives');
+            data.addColumn('number', 'Euclidean False Positives');
+            data.addColumn('number', 'Segment Checking Euclidean True Positives');
+            data.addColumn('number', 'Segment Checking Euclidean False Positives');
             data.addRows([
 ";
 
 while ($type_row = $type_result->fetch_assoc()) {
     $type_id = $type_row['id'];
     $type_name = $type_row['name'];
-    $timed_query = "SELECT id, video_id, species_id FROM timed_observations AS t WHERE expert = 0 AND event_id = $type_id AND start_time > 0 AND end_time > start_time AND EXISTS (SELECT * FROM timed_observations AS i WHERE t.video_id = i.video_id AND i.expert = 1 AND i.start_time > 0 AND i.end_time > i.start_time)";
+    $timed_query = "SELECT id, video_id FROM timed_observations AS t WHERE expert = 0 AND event_id = $type_id AND start_time > 0 AND end_time > start_time AND EXISTS (SELECT * FROM timed_observations AS i WHERE t.video_id = i.video_id AND i.expert = 1 AND i.start_time > 0 AND i.end_time > i.start_time)";
     $timed_result = query_wildlife_video_db($timed_query);
-    $species_num_events = array();
-    $species_match_events = array();
-    foreach($species as $s_id => $s_name) {
-        $species_num_events[$s_id] = 0;
-        $species_match_events[$s_id] = 0;
-    }
+    $num_events = $timed_result->num_rows;
+    $buffer_true_positives = 0;
+    $buffer_false_positives = 0;
+    $euclidean_true_positives = 0;
+    $euclidean_false_positives = 0;
+    $segmented_euclidean_true_positives = 0;
+    $segmented_euclidean_false_positives = 0;
     while ($timed_row = $timed_result->fetch_assoc()) {
         $obs_id = $timed_row['id'];
         $video_id = $timed_row['video_id'];
-        $species_id = $timed_row['species_id'];
 
         $expert_query = "SELECT user_id FROM timed_observations WHERE video_id = $video_id AND expert = 1 LIMIT 1";
         $expert_result = query_wildlife_video_db($expert_query);
@@ -92,40 +80,46 @@ while ($type_row = $type_result->fetch_assoc()) {
             $expert_id = $expert_row['user_id'];
         }
 
-        //$correctness = getBufferCorrectness($obs_id, $expert_id, $buffer);
-        list($correctness, $specificity) = getEuclideanCorrectness($obs_id, $expert_id, $threshold);
-        //$correctness = getSegmentedEuclideanCorrectness($obs_id, $expert_id, $threshold);
+        list($buffer_correctness, $buffer_specificity) = getBufferCorrectness($obs_id, $expert_id, $buffer);
+        list($euclidean_correctness, $euclidean_specificity) = getEuclideanCorrectness($obs_id, $expert_id, $threshold);
+        list($segmented_euclidean_correctness, $segmented_euclidean_specificity) = getSegmentedEuclideanCorrectness($obs_id, $expert_id, $threshold);
 
-        $species_num_events[$species_id] += 1;
+        if ($euclidean_specificity) {
+            $euclidean_true_positives += 1;
+        } else {
+            $euclidean_false_positives += 1;
+        }
+        
+        if ($segmented_euclidean_specificity) {
+            $segmented_euclidean_true_positives += 1;
+        } else {
+            $segmented_euclidean_false_positives += 1;
+        }
 
-        if ($specificity) {
-            $species_match_events[$species_id] += $correctness;
+        if ($buffer_specificity) {
+            $buffer_true_positives += 1;
+        } else {
+            $buffer_false_positives += 1;
         }
     }
 
-    $add_data = false;
-    foreach($species_match_events as $s_id => $s_val) {
-        if ($s_val > 0) {
-            $add_data = true;
-        }
-    }
-
-    if ($add_data) {
-        echo "[";
-        echo "'$type_name'";
-        $tooltip = "Okay";
-        foreach($species_match_events as $s_id => $s_val) {
-            echo ",";
-            if ($species_num_events[$s_id] > 0) {
-                echo $s_val / $species_num_events[$s_id] * 100;
-            } else {
-                echo "0";
-            }
-        }
-        echo ",";
-        echo "'$tooltip'";
-        echo "],";
-    }
+    echo "[";
+    echo "'$type_name'";
+    echo ",";
+    echo $num_events;
+    echo ",";
+    echo $buffer_true_positives;
+    echo ",";
+    echo $buffer_false_positives;
+    echo ",";
+    echo $euclidean_true_positives;
+    echo ",";
+    echo $euclidean_false_positives;
+    echo ",";
+    echo $segmented_euclidean_true_positives;
+    echo ",";
+    echo $segmented_euclidean_false_positives;
+    echo "],";
 }
 
 echo "
@@ -134,35 +128,30 @@ echo "
 ";
 echo "
             var options = {
-                title: 'Percent of correct events for each type by species',
-                hAxis: {title: 'Event Type'},
-                vAxis: {
-                    title: 'Percent Correct',
-                    maxValue: 100,
-                }
+                title: 'Percent of correct events for each type'
             };
 
-            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+            var chart = new google.visualization.Table(document.getElementById('chart_div'));
 
             chart.draw(data, options);
         }
     </script>
 
-            <h1>Correctness by Event Type and Species</h1>
+            <h1>Correctness by Type as a Table</h1>
 
-            <div id='chart_div' style='margin: auto; width: 90%; height: 500px;'></div>
+            <div id='chart_div' style='margin: auto; width: auto; height: auto;'></div>
 
             <h2>Parameters: (portion of the URL after a '?')</h2>
             <dl>
                 <dt>buffer=</dt>
                 <dd>The error in either direction allowed for two events to be matched. The default value is 5.</dd>
+                <dt>threshold=</dt>
+                <dd>The correctness required to count two events as a true match. The default value is 95%.</dd>
             </dl>
-            
 
             <h2>Description:</h2>
-            <p>This bar chart show the percentage of user events that have a matching expert observed event. Each bar represens the percent of events that match an expert observation. The legent shows the breakdown for each species.</p>
+            <p>This table shows the number of true positives and false positives for each event type and algorithm.</p>
             <p>In order to collect this data we discard all vidoes that do not have an expert observation or the expert observation is invalid. This is done by getting a list of all event types and then counting the total number of user events that have a matchins event and dividing it by the number of user events of that type that have an valid expert observation for that video.</p>
-
         </div>
     </div>
 </div>
