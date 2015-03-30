658191aaee75ed3c950f867d8f494b94835efca1
Gylo <benjamin.carpenter@my.und.edu>
2015-03-30 16:50:14 -0500

    Project changes from url and alert changed to modal
    Species and id are now correctly read from the new database tables.

--- webpage/canvas_test.php
+++ webpage/canvas_test.php
@@ -54,7 +54,8 @@ container_end();
 
 
 $image_id = -1;
-$project_id = 1;
+$project_id = $_GET['p'];
+if (!$project_id) $project_id = 1;
 
 $result = NULL;
 //TODO Update query so user doesn't see verified image - BCC
@@ -106,7 +107,27 @@ echo "
     <div class='col-sm-8' onselectstart='return false' ondragstart='return false'>
         <div id='canvas'>
             <img class='img-responsive' id='$image_id'  src='http://wildlife.und.edu/$image'></img>
-        </div>";
+        </div>
+    </div>
+</div>
+	<div id='submitModal' class='modal fade'>
+		<div class='modal-dialog' role='dialog'>
+			<div class='modal-content'>
+				<div class='modal-header'>
+					<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>
+						&times;
+					</button>
+					<h4 class='modal-title''>Submission Complete</h4>
+					<div class='modal-body'>
+						<p>Thx!</p>
+					</div>
+					<div class='modal-footer'>
+						<button id='modalSubButton' type='button' class='btn btn-primary' data-dismiss='modal'>Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	<div>";
 
 
 print_footer();
