c1f129fe1adda97585718797e4c3fcf51bfdf5fc
Marshall Mattingly <marshall.p.mattingly@und.edu>
2016-03-23 11:05:12 -0500

    fully working submission

--- webpage/js/canvas_test.js
+++ webpage/js/canvas_test.js
@@ -12,7 +12,7 @@ var newRectCallback = function(id) {
             "<div class='input-group'>" +
                 "<input type='text' class='form-control disabled' value='On Nest?' disabled>" +
                 "<span class='input-group-addon'>" +
-                    "<input type='checkbox'>" +
+                    "<input type='checkbox' id='nest" + id + "'>" +
                 "</span>" +
             "</div>" +
             "<div class='input-group hidden' id='otherdiv" + id + "'>" +
@@ -83,6 +83,75 @@ $(document).ready(function() {
             });
         }
     });
+
+    $('#submitForm').submit(function(e) {
+        // add the info for each rectangle
+        var boxes = [];
+        cs.rectangles.forEach(function(e) {
+            var species = $("#species" + e.id + " option:selected")[0].value;
+            var nest = $("#nest" + e.id)[0].checked ? 1 : 0;
+
+            boxes.push({
+                'x': e.left,
+                'y': e.top,
+                'width': e.width,
+                'height': e.height,
+                'species_id': species,
+                'on_nest': nest
+            });
+        });
+
+        var formData = {
+            'metadata': {
+                'image_id' : $('input[name=image_id]').val(),
+                'comments' : '',
+                'start_time': $('input[name=submitStart]').val(),
+                'nothing_here': cs.rectangles.length == 0 ? 1 : 0
+            },
+            'boxes': boxes
+        };
+
+        console.log(formData);
+
+        // process
+        $.ajax({
+            type: 'POST',
+            url: 'canvas_submission.php',
+            data: formData,
+            dataType: 'json',
+            encode: true
+        })
+            .done(function(data) {
+                if (!data['success']) {
+                    $("#ajaxalert").removeClass('alert-success');
+                    $("#ajaxalert").addClass('alert-danger');
+                    $("#ajaxalert").html("<strong>Error!</strong> " + data['errors']);
+                    $("#ajaxalert").removeClass('hidden');
+                } else {
+                    $("#ajaxalert").removeClass('alert-danger');
+                    $("#ajaxalert").addClass('alert-success');
+
+                    var html = '';
+                    if (data['count'] == 0) html = 'Nothing here submitted.';
+                    else if (data['count'] == 1) html = '1 rectangle added.';
+                    else html = data['count'] + ' rectangles added.';
+
+                    $("#ajaxalert").html("<strong>Success!</strong> " + html + ' Reloading page in 5 seconds.');
+                    $("#ajaxalert").removeClass('hidden');
+
+                    setTimeout(function() {
+                        location.reload();
+                    }, 5000);
+                }
+            });
+
+            //.fail(function(data) {
+            //    console.log(data);
+            //});
+        
+        // stop normal submission
+        e.preventDefault();
+    });
 });
 
 $("#discuss-button").click(function() {
@@ -91,17 +160,14 @@ $("#discuss-button").click(function() {
 });
 
 $("#skip-button").click(function() {
-    $("#submitSuccess").val(0);
-    $("#submitForm").submit();
+    location.reload();
 });
 
 $("#nothing-here-button").click(function() {
-    // TODO: SAVE
     $("#submitForm").submit();
 });
 
 $("#submit-selections-button").click(function() {
-    // TODO: SAVE
     $("#submitForm").submit();
 });
 
--- webpage/trailcam_image.php
+++ webpage/trailcam_image.php
@@ -19,36 +19,26 @@ print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
 $image_id = -1;
 $project_id = $_GET[('p')];
 if(!$project_id) $project_id=1;
-$query = "select id, watermarked_filename, watermarked, species, year from (select id, watermarked_filename, watermarked, species, year from images where watermarked=1 and views < needed_views and project_id=$project_id order by rand() limit 1000) as t1 where id != any (select image_id from test_image_observations where user_id=$user_id) limit 1";
-
-$temp_result = query_wildlife_video_db("select max(id), min(id) from images");
-$row = $temp_result->fetch_assoc();
-$max_int = $row['max(id)'];
-$min_int = $row['min(id)'];
-
-do {
-	$temp_id = mt_rand($min_int, $max_int);
-	$temp_result = query_wildlife_video_db("select id, watermarked_filename, watermarked, species, year from images where watermarked=1 and views < needed_views and project_id=$project_id and id not in (select image_id from test_image_observations where user_id=$user_id) and id=$temp_id");
-} while ($temp_result->num_rows < 1);
 
 $result = NULL;
-//TODO Update query so user doesn't see verified image - BCC
 if (array_key_exists('image_id', $_GET)) {
     $image_id = $boinc_db->real_escape_string($_GET['image_id']);
     $result = query_wildlife_video_db("SELECT id, watermarked_filename, watermarked, species, year FROM images WHERE id = $image_id");
-
 } else {
-    //$result = query_wildlife_video_db($query);
+    // TODO: This seems *very* inefficient. Work on optimizing this query.
+    $temp_result = query_wildlife_video_db("select max(id), min(id) from images");
+    $row = $temp_result->fetch_assoc();
+    $max_int = $row['max(id)'];
+    $min_int = $row['min(id)'];
+
+    do {
+        $temp_id = mt_rand($min_int, $max_int);
+        $temp_result = query_wildlife_video_db("select id, watermarked_filename, watermarked, species, year from images where watermarked=1 and views < needed_views and project_id=$project_id and id not in (select image_id from image_observations where user_id=$user_id) and id=$temp_id");
+    } while ($temp_result->num_rows < 1);
+
     $result = $temp_result;
-    //$row = $result->fetch_assoc();
 
-    //from citizen_science_grid/my_query.php
-    //this is supposedly much faster, as per: http://akinas.com/pages/en/blog/mysql_random_row/
-    //$offset_result = query_wildlife_video_db( " SELECT FLOOR(RAND() * COUNT(*)) AS `offset` FROM images");
-    //$offset_row = $offset_result->fetch_assoc();
-    //$image_id = $offset_row['offset'];
-
-    //$result = query_wildlife_video_db("SELECT id, watermarked_filename, watermarked FROM images LIMIT $image_id, 1 ");
+    //$query = "select id, watermarked_filename, watermarked, species, year from (select id, watermarked_filename, watermarked, species, year from images where watermarked=1 and views < needed_views and project_id=$project_id order by rand() limit 1000) as t1 where id != any (select image_id from image_observations where user_id=$user_id) limit 1";
 }
 
 $row = $result->fetch_assoc();
@@ -56,29 +46,15 @@ $row = $result->fetch_assoc();
 $image_id = $row['id'];
 $image_watermarked = $row['watermarked'];
 $image = $row['watermarked_filename'];
-$species_id = $row['species'];
 $year = $row['year'];
-if($species_id == 3)
-{ $species = "predator";}
-else if($species_id == 2)
-{ $species = "lesser snow goose";}
-else if($species_id == 1)
-{ $species = "common eider";}
-else
-{ $species = "other";}
-
-//TODO make 'nothing here' button work -Jaeden
 
 $alert_class = 'hidden';
-if (isset($_POST['submitSuccess']) and $_POST['submitSuccess']) {
-    $alert_class = 'alert-success';
-}
 
 echo "
 <div class='container-fluid'>
 <div class='row'>
     <div class='col-sm-12'>
-        <div class='alert $alert_class' role='alert'>
+        <div class='alert $alert_class' role='alert' id='ajaxalert'>
             <strong>Success!</strong> Data submited to the database.
         </div>
     </div>
@@ -92,7 +68,7 @@ echo "
                         <button type='button' class='btn disabled' disabled><strong>Image #: $image_id</strong></button>
                         <button type='button' id='discuss-button' class='btn btn-primary'>&nbsp;<span class='glyphicon glyphicon-comment'> </span></button>
                     </div>
-                    <!-- You are looking at image: $image_id and it is watermarked? $image_watermarked. <br>Species: $species. Year: $year. <br> $image Image ID: $image_id -->
+                    <!-- You are looking at image: $image_id and it is watermarked? $image_watermarked. <br>Year: $year. <br> $image Image ID: $image_id -->
                     <div class='btn-group btn-group-sm pull-right' role='group'>
                         <button type='button' class='btn btn-info' data-toggle='modal' data-target='#helpModal'>Species <span class='glyphicon glyphicon-question-sign'> </span></button>
                         <button type='button' class='btn btn-info' data-toggle='modal' data-target='#interfaceModal'>Interface <span class='glyphicon glyphicon-question-sign'> </span></button>
@@ -240,7 +216,9 @@ echo "<div id='submitModal' class='modal fade' data-backdrop='static'>
 
 echo "
 <form class='hidden' action='' method='POST' id='submitForm'>
-    <input type='hidden' id='submitSuccess' name='submitSuccess' value='1'/>
+<input type='hidden' id='submitStart' name='submitStart' value='".time()."'/>
+<input type='hidden' id='submitEnd' name='submitEnd' value='0'/>
+<input type='hidden' id='image_id' name='image_id' value='$image_id'/>
 </form>
     
 <form id='forumPost' class='hidden' action='//csgrid.org/csg/forum_post.php?id=8' method='post' target='_blank'>
