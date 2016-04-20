b8768e079c194c2e8765e5a3fd15a5878fe21ec3
lindseywingate <lindseymwingate@gmail.com>
2016-04-20 11:04:58 -0500

    got k5 module working with all videos (except LOST videos, need separate template)

--- webpage/k5_module.php
+++ webpage/k5_module.php
@@ -95,6 +95,132 @@ $wells['well'][] = array('video' => "clips_grouse/BEL_2012/150.462_48.10577_102.
 			array("item1" => "third", "item2" => "sixth")
 		)
 	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/148.203_48.13689_102.38178",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/148.222_48.11228_102.44405",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.052_48.13511_102.38803",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.554_48.11663_102.47623.",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.573_48.1223_102.3886",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.573_48.12436_102.38857",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.744_48.145075_102.39126",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.783_48.12209_102.38947",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.793_48.14372_102.38706",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.801_48.075062_102.380261",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/149.823_48.13639_102.3874",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/150.242_48.11884_102.38034",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/150.261_48.11042_102.44061",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/150.342_48.10705_102.41093",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/150.432_48.10356_102.430624",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/151.564_48.1238_102.39722",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/151.644_48.13074_102.44943",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
+$wells['well'][] = array('video' => "clips_grouse/BEL_2013/151.675_48.10591_102.44745",
+		"list" => array(
+			array("item1" => "BEL_2013", "item2" => "fourth"),
+			array("item1" => "third", "item2" => "fifth"),
+			array("item1" => "third", "item2" => "sixth")
+		)
+	);
 $wells['well'][] = array('video' => "clips_grouse/BLA_2012/148.574_48.27494_102.05381",
 		"list" => array(
 			array("item1" => "BLA_2012", "item2" => "fourth"),
@@ -249,121 +375,189 @@ $wells['well'][] = array('video' => "clips_grouse/BLA_2012/151.664_48.27504_102.
 			array("item1" => "third", "item2" => "sixth")
 		)
 	);
-$wells2 = array();
-$wells2['well2'][] = array('video2'=>"clips_grouse/BEL_2013/148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "BLA_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/148.203_48.13689_102.38178",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"clips_grouse/BEL_2013/148.222_48.11228_102.44405",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/148.222_48.11228_102.44405",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"clips_grouse/BEL_2013/149.052_48.13511_102.38803",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.052_48.13511_102.38803",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"clips_grouse/BEL_2013/149.554_48.11663_102.47623",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.554_48.11663_102.47623",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.573_48.1223_102.3886",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.573_48.12436_102.38857",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.744_48.145075_102.39126",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.783_48.12209_102.38947",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.793_48.14372_102.38706",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/ 149.801_48.075062_102.380261",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/149.823_48.13639_102.3874",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/150.242_48.11884_102.38034",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/150.261_48.11042_102.44061",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/150.342_48.10705_102.41093",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/150.432_48.10356_102.430624",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/151.564_48.1238_102.39722",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/151.644_48.13074_102.44943",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
-$wells2['well2'][] = array('video2'=>"148.203_48.13689_102.38178",
-		"list2" => array(
-			array("item3" => "bla_2013", "item4" => "test"),
-			array("item3" => "next", "item4" => "test2")
+$wells['well'][] = array('video'=>"clips_grouse/BEL_2013/151.675_48.10591_102.44745",
+		"list" => array(
+			array("item1" => "BLA_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
 		)
 	);
+/*
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/148.043_48.52878_102.49752",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/148.473_48.65523_102.43999",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/49.503_48.65188_102.43567",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/149.554_48.52755_102.49201",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/149.564_48.61229_102.43444",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/149.654_48.64902_102.44421",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/150.512_48.53613_102.42688",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/150.622_48.52777_102.45663",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/150.832_48.52606_102.47702",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/151.632_48.65865_102.42151",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+$wells['well'][] = array('video'=>"clips_grouse/LOST_2013/151.683_48.52316_102.47265",
+		"list" => array(
+			array("item1" => "LOST_2013", "item2" => "test"),
+			array("item1" => "next", "item2" => "test2")
+		)
+	);
+*/
 
 $k5 = file_get_contents($cwd[__file__] . "/templates/k5.html");
 
 $m = new mustache_engine;
 echo $m->render($k5, $wells);	
-echo $m->render($k5, $wells2);
+echo $m->render($k5, $wells);
 print_footer('travis desell, susan ellis-felege, lindsey wingate and the wildlife@home team', 'travis desell, susan ellis-felege');
 
 echo "</body></html>";
