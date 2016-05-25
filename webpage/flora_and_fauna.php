3dda6b30c20e9a3e4390aec60188e2df153ac3d9
lindseywingate <lindseymwingate@gmail.com>
2016-05-25 13:56:12 -0500

    added flora and fauna

---
+++ webpage/flora_and_fauna.php
@@ -0,0 +1,24 @@
+<?php
+
+$cwd[__FILE__] = __FILE__;
+if (is_link($cwd[__FILE__])) $cwd[__FILE__] = readlink($cwd[__FILE__]);
+$cwd[__FILE__] = dirname($cwd[__FILE__]);
+
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/header.php");
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/navbar.php");
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/news.php");
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/footer.php");
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/my_query.php");
+require_once($cwd[__FILE__] . "/../../citizen_science_grid/csg_uotd.php");
+
+
+$css_header = "<link rel='stylesheet' type='text/css' href='wildlife_css/education_style.css'/>";
+
+print_header("Wildlife@Home: Teaching & Learning", "$css_header", "wildlife");
+print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
+
+print_footer('Travis Desell, Susan Ellis-Felege, Lindsey Wingate and the Wildlife@Home Team', 'Travis Desell, Susan Ellis-Felege');
+
+echo "</body></html>";
+
+?>
