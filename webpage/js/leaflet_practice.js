a6fdafb1dc1845bbcbcc0d7ed162eb83325eff88
lindseywingate <lindseymwingate@gmail.com>
2016-08-31 22:19:45 -0500

    working on leaflet still

---
+++ webpage/js/leaflet_practice.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	var mymap=L.map('mapid').setView([51.505, -0.09], 13);
+});
--- webpage/leaflet_practice.php
+++ webpage/leaflet_practice.php
@@ -6,22 +6,26 @@ $cwd[__FILE__] = dirname($cwd[__FILE__]);
 
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/header.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/navbar.php");
-require_once($cwd[__FILE__] . "/../../citizen_science_grid/news.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/footer.php");
 require_once($cwd[__FILE__] . "/../../citizen_science_grid/my_query.php");
-require_once($cwd[__FILE__] . "/../../citizen_science_grid/csg_uotd.php");
 
+$leaflet_css = "<link rel='stylesheet' type='text/css' href='wildlife_css/leaflet.css";
+$leaflet_js ="<script type='text/javascript' src='js/leaflet_practice.js'></script>";
 
-$css_header = "<link rel='stylesheet' type='text/css' href='wildlife_css/education_style.css'/>";
-
-print_header("Wildlife@Home: Teaching & Learning", "$css_header", "wildlife");
+print_header("Wildlife@Home: Teaching & Learning", "$leaflet_css, $leaflet_js,<link rel='stylesheet' href='https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.css' /> <script src='https://npmcdn.com/leaflet@1.0.0-rc.3/dist/leaflet.js'></script>", "wildlife");
 print_navbar("Projects: Wildlife@Home", "Wildlife@Home", "..");
 
-
-
-
+echo "
+<div class='container'>
+	<div class='row'>
+		<div class='col-sm-12'>
+			<div id='mapid'></div>
+		</div>
+	</div>
+</div>
+";
 
 print_footer('Travis Desell, Susan Ellis-Felege, Lindsey Wingate and the Wildlife@Home Team', 'Travis Desell, Susan Ellis-Felege');
 
-//echo "</body></html>";
+echo "</body></html>";
 ?>
